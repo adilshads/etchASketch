c635df571310f61da6056686b482947e66e6f064
Adil Shad <115205289+adilshads@users.noreply.github.com>
2023-05-16 15:40:15 -0400

    Create function to generate grid

--- script.js
+++ script.js
@@ -10,6 +10,24 @@ let discoveredPlanets = []; // An array to track the discovered planets and the 
 let rocketCursor; // The rocket ship cursor image element.
 let isRocketCursorActive; // A flag to indicate if the rocket ship cursor is active.
 
-
-
-
+function generateGrid(gridSize) {
+    const container = document.createElement('div');
+    container.style.width = '960px';
+    document.body.appendChild(container);
+  
+    const squareSize = container.offsetWidth / gridSize;
+  
+    for (let row = 0; row < gridSize; row++) {
+      for (let column = 0; column < gridSize; column++) {
+        const square = document.createElement('div');
+        square.classList.add('square');
+        square.style.width = `${squareSize}px`;
+        square.style.height = `${squareSize}px`;
+        square.addEventListener('mouseenter', handleDrawing);
+        container.appendChild(square);
+      }
+    }
+  }
+  
+  generateGrid(gridSize);
+  
