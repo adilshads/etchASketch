9bae406d9ec19e39f7ef84f17a08ee48754c7802
Adil Shad <115205289+adilshads@users.noreply.github.com>
2023-05-16 00:30:08 -0400

    Innitialize variables

---
+++ script.js
@@ -0,0 +1,11 @@
+/** Initialize Variables */
+
+let gridSize; // Number of squares in each row and column of the grid. 
+let container; // Container element displaying grid. 
+let colorButton; // Drawing color.
+let isDrawing; // Current Drawing color.
+let eraseButton; // Returns grid color to white. 
+let clearButton; // Clears entire grid from color. 
+let discoveredPlanets;// An array to track the discovered planets and the sun. 
+let rocketCursor; // The rocket ship cursor image element.
+let isRocketCursorActive; // A flag to indicate if the rocket ship cursor is active.
